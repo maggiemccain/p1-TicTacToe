a9cee88c2b489df7ca2abf97e6c52ab4673445f0
Maggie McCain <maggie.mccain13@gmail.com>
2016-07-14 21:47:56 +1000

    deleted images, added score and tie to js, css updates

--- apps2.js
+++ apps2.js
@@ -1,8 +1,11 @@
-console.log('second attempt');
 var $resetBtn = $('#resetBtn');
 var turn = true; //cat turn
 var $cards = $('.card');
 var $currentPlayer = $('#currentPlayer');
+var scoreCat = 0;
+var scoreDog = 0;
+var $catScoreboard = $('#catNum');
+var $dogScoreboard = $('#dogNum');
 
 function reset() {
   $('.card').removeClass('firstPlayerCat');
@@ -10,9 +13,19 @@ function reset() {
   $('.playerDog').removeClass('currentPlayer');
   $('.playerCat').addClass('currentPlayer');
   turn = true;
-  player = [];
-  opp = [];
 }
+
+function updateCatScore() {
+  $catScoreboard.html(scoreCat+=1);
+  reset();
+}
+
+function updateDogScore() {
+  $dogScoreboard.html(scoreDog+=1);
+  reset();
+}
+
+$('.playerCat').addClass('currentPlayer');
 
 $cards.on('click', function(event){
   if (turn === true) {
@@ -24,7 +37,7 @@ $cards.on('click', function(event){
     setTimeout(function(){
       $('.playerDog').addClass('currentPlayer');
       $('.playerCat').removeClass('currentPlayer');
-    }, 200);
+    }, 100);
   } else if (turn === false){
       var $cardClickedId = event.target.id;
       var $cardClicked = $(event.target);
@@ -34,61 +47,63 @@ $cards.on('click', function(event){
       setTimeout(function(){
         $('.playerDog').removeClass('currentPlayer');
         $('.playerCat').addClass('currentPlayer');
-      }, 200);
+      }, 100);
   }
 })
 
 function checkWinner() {
-    // if ($('box1') && $('box2') && $('box3').hasClass('firstPlayerCat')
     if ($('#box1').hasClass('firstPlayerCat') && $('#box2').hasClass('firstPlayerCat') && $('#box3').hasClass('firstPlayerCat')) {
       alert ('cats rule, dog drools');
-      reset();
+      updateCatScore();
     } else if ($('#box4').hasClass('firstPlayerCat') && $('#box5').hasClass('firstPlayerCat') && $('#box6').hasClass('firstPlayerCat')) {
-      alert ('cats rule, dog drools');
-      reset();
+        alert ('cats rule, dog drools');
+        updateCatScore();
     } else if ($('#box7').hasClass('firstPlayerCat') && $('#box8').hasClass('firstPlayerCat') && $('#box9').hasClass('firstPlayerCat')) {
-      alert ('cats rule, dog drools');
-      reset();
+        alert ('cats rule, dog drools');
+        updateCatScore();
     } else if ($('#box1').hasClass('firstPlayerCat') && $('#box4').hasClass('firstPlayerCat') && $('#box7').hasClass('firstPlayerCat')) {
-      alert ('cats rule, dog drools');
-      reset();
+        alert ('cats rule, dog drools');
+        updateCatScore();
     } else if ($('#box2').hasClass('firstPlayerCat') && $('#box5').hasClass('firstPlayerCat') && $('#box8').hasClass('firstPlayerCat')) {
-      alert ('cats rule, dog drools');
-      reset();
+        alert ('cats rule, dog drools');
+        updateCatScore();
     } else if ($('#box3').hasClass('firstPlayerCat') && $('#box6').hasClass('firstPlayerCat') && $('#box9').hasClass('firstPlayerCat')) {
-      alert ('cats rule, dog drools');
-      reset();
+        alert ('cats rule, dog drools');
+        updateCatScore();
     } else if ($('#box1').hasClass('firstPlayerCat') && $('#box5').hasClass('firstPlayerCat') && $('#box9').hasClass('firstPlayerCat')) {
-      alert ('cats rule, dog drools');
-      reset();
+        alert ('cats rule, dog drools');
+        updateCatScore();
     } else if ($('#box3').hasClass('firstPlayerCat') && $('#box5').hasClass('firstPlayerCat') && $('#box7').hasClass('firstPlayerCat')) {
-      alert ('cats rule, dog drools');
-      reset();
+        alert ('cats rule, dog drools');
+        updateCatScore();
     } else if ($('#box4').hasClass('oppDog') && $('#box5').hasClass('oppDog') && $('#box6').hasClass('oppDog')) {
-      alert ('dogs rule, cats drools');
-      reset();
+        alert ('dogs rule, cats drools');
+        updateDogScore();
     } else if ($('#box1').hasClass('oppDog') && $('#box2').hasClass('oppDog') && $('#box3').hasClass('oppDog')) {
-      alert ('dogs rule, cats drools');
-      reset();
+        alert ('dogs rule, cats drools');
+        updateDogScore();
     } else if ($('#box7').hasClass('oppDog') && $('#box8').hasClass('oppDog') && $('#box9').hasClass('oppDog')) {
-      alert ('dogs rule, cats drools');
-      reset();
+        alert ('dogs rule, cats drools');
+        updateDogScore();
     } else if ($('#box1').hasClass('oppDog') && $('#box4').hasClass('oppDog') && $('#box7').hasClass('oppDog')) {
-      alert ('dogs rule, cats drools');
-      reset();
+        alert ('dogs rule, cats drools');
+        updateDogScore();
     } else if ($('#box2').hasClass('oppDog') && $('#box5').hasClass('oppDog') && $('#box8').hasClass('oppDog')) {
-      alert ('dogs rule, cats drools');
-      reset();
+        alert ('dogs rule, cats drools');
+        updateDogScore();
     } else if ($('#box3').hasClass('oppDog') && $('#box6').hasClass('oppDog') && $('#box9').hasClass('oppDog')) {
-      alert ('dogs rule, cats drools');
-      reset();
+        alert ('dogs rule, cats drools');
+        updateDogScore();
     }  else if ($('#box1').hasClass('oppDog') && $('#box5').hasClass('oppDog') && $('#box9').hasClass('oppDog')) {
-      alert ('dogs rule, cats drools');
-      reset();
+        alert ('dogs rule, cats drools');
+        updateDogScore();
     } else if ($('#box3').hasClass('oppDog') && $('#box5').hasClass('oppDog') && $('#box7').hasClass('oppDog')) {
-      alert ('dogs rule, cats drools');
-      reset();
-    }
+        alert ('dogs rule, cats drools');
+        updateDogScore();
+    } else if ($('.firstPlayerCat').length + $('.oppDog').length === 9) {
+        alert('tic tac tie');
+        reset();
+     }
 }
 
 $resetBtn.on('click', reset);
