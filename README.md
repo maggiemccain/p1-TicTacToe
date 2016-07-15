0b663d55d6a4a02a61e12a8021a574d97f7def39
Maggie McCain <maggie.mccain13@gmail.com>
2016-07-15 14:01:52 +1000

    add README.md, updated win and lose messaging

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 HTML, CSS, and Javascript/jQuery library were used for this assignment.  
 
 #My Approach
-I started off with basic HTML layout for the game board and then moved straight into working on logic first.  I hardcoded one player to feel out the functions and scoring.  I ended up using a chain of functions set off by an event listener (click).  A click on the game board applied a class to the DOM element designating which player clicked, then a function would check for a winner and if it didn't find one it would switch ownership of the board to player 2.  Eventually the function would find a winner or call a tie and reset the game board.  
+I started off with basic HTML layout for the game board and then on javascript first.  I hardcoded one player to feel out the functions and scoring.  I ended up using a chain of functions set off by an event listener (click).  A click on the game board applied a class to the DOM element designating which player clicked, then a function would check for a winner, and if it didn't find one it would switch ownership of the board to player 2.  Eventually the function would find a winner or call a tie and reset the game board.  
 
 #Installation instructions
+Visit my Github at https://github.com/maggiemccain/ and fork the "p1-TicTacToe" repo to install.  It is also available on at 
 
 #Unsolved problems, etc.
 
 - CSS- I need to do more planning before I begin touch the css file.  It was hard to backtrack after I'd done initial styling.  I also didn't consider scalability early in the process, which became problematic when I attempted to make the layout responsive.  I wish I'd styled it in a more simple fashion.  
 - Javascript- When I was thinking through the steps of the game and the logic behind it, I quickly realized my plan of attack was not the most straightforward option, but couldn't fully grasp the alternative paths so I chose to move forward with my original plan.  
-- Because of the way I chose to write the logic, its not DRY.  
+- Because of the way I chose to write the logic, its not DRY.
 - HTML- I should have used more divs from the beginning for ease of styling.
--- apps2.js
+++ apps2.js
@@ -6,6 +6,10 @@ var scoreCat = 0;
 var scoreDog = 0;
 var $catScoreboard = $('#catNum');
 var $dogScoreboard = $('#dogNum');
+var $catWin = $('#catWin');
+var $dogWin = $('#dogWin');
+var $gameboard = $('#gameboard');
+var $tie = $('#tie');
 
 function reset() {
   $('.card').removeClass('firstPlayerCat');
@@ -13,6 +17,41 @@ function reset() {
   $('.playerDog').removeClass('currentPlayer');
   $('.playerCat').addClass('currentPlayer');
   turn = true;
+}
+
+function catWinMsg() {
+  setTimeout(function() {
+    $catWin.show();
+    updateCatScore();
+    $gameboard.hide();
+  }, 500);
+  setTimeout(function() {
+    $catWin.hide();
+    $gameboard.show();
+  }, 2000);
+}
+
+function dogWinMsg() {
+  setTimeout(function() {
+    $dogWin.show();
+    updateDogScore();
+    $gameboard.hide();
+  }, 500);
+  setTimeout(function() {
+    $dogWin.hide();
+    $gameboard.show();
+  }, 2000);
+}
+
+function tieMsg() {
+  setTimeout(function() {
+    $tie.show();
+    $gameboard.hide();
+  }, 100);
+  setTimeout(function() {
+    $tie.hide();
+    $gameboard.show();
+  }, 2000);
 }
 
 function updateCatScore() {
@@ -52,58 +91,42 @@ $cards.on('click', function(event){
 })
 
 function checkWinner() {
-    if ($('#box1').hasClass('firstPlayerCat') && $('#box2').hasClass('firstPlayerCat') && $('#box3').hasClass('firstPlayerCat')) {
-      alert ('cats rule, dog drools');
-      updateCatScore();
-    } else if ($('#box4').hasClass('firstPlayerCat') && $('#box5').hasClass('firstPlayerCat') && $('#box6').hasClass('firstPlayerCat')) {
-        alert ('cats rule, dog drools');
-        updateCatScore();
-    } else if ($('#box7').hasClass('firstPlayerCat') && $('#box8').hasClass('firstPlayerCat') && $('#box9').hasClass('firstPlayerCat')) {
-        alert ('cats rule, dog drools');
-        updateCatScore();
-    } else if ($('#box1').hasClass('firstPlayerCat') && $('#box4').hasClass('firstPlayerCat') && $('#box7').hasClass('firstPlayerCat')) {
-        alert ('cats rule, dog drools');
-        updateCatScore();
-    } else if ($('#box2').hasClass('firstPlayerCat') && $('#box5').hasClass('firstPlayerCat') && $('#box8').hasClass('firstPlayerCat')) {
-        alert ('cats rule, dog drools');
-        updateCatScore();
-    } else if ($('#box3').hasClass('firstPlayerCat') && $('#box6').hasClass('firstPlayerCat') && $('#box9').hasClass('firstPlayerCat')) {
-        alert ('cats rule, dog drools');
-        updateCatScore();
-    } else if ($('#box1').hasClass('firstPlayerCat') && $('#box5').hasClass('firstPlayerCat') && $('#box9').hasClass('firstPlayerCat')) {
-        alert ('cats rule, dog drools');
-        updateCatScore();
-    } else if ($('#box3').hasClass('firstPlayerCat') && $('#box5').hasClass('firstPlayerCat') && $('#box7').hasClass('firstPlayerCat')) {
-        alert ('cats rule, dog drools');
-        updateCatScore();
-    } else if ($('#box4').hasClass('oppDog') && $('#box5').hasClass('oppDog') && $('#box6').hasClass('oppDog')) {
-        alert ('dogs rule, cats drools');
-        updateDogScore();
-    } else if ($('#box1').hasClass('oppDog') && $('#box2').hasClass('oppDog') && $('#box3').hasClass('oppDog')) {
-        alert ('dogs rule, cats drools');
-        updateDogScore();
-    } else if ($('#box7').hasClass('oppDog') && $('#box8').hasClass('oppDog') && $('#box9').hasClass('oppDog')) {
-        alert ('dogs rule, cats drools');
-        updateDogScore();
-    } else if ($('#box1').hasClass('oppDog') && $('#box4').hasClass('oppDog') && $('#box7').hasClass('oppDog')) {
-        alert ('dogs rule, cats drools');
-        updateDogScore();
-    } else if ($('#box2').hasClass('oppDog') && $('#box5').hasClass('oppDog') && $('#box8').hasClass('oppDog')) {
-        alert ('dogs rule, cats drools');
-        updateDogScore();
-    } else if ($('#box3').hasClass('oppDog') && $('#box6').hasClass('oppDog') && $('#box9').hasClass('oppDog')) {
-        alert ('dogs rule, cats drools');
-        updateDogScore();
-    }  else if ($('#box1').hasClass('oppDog') && $('#box5').hasClass('oppDog') && $('#box9').hasClass('oppDog')) {
-        alert ('dogs rule, cats drools');
-        updateDogScore();
-    } else if ($('#box3').hasClass('oppDog') && $('#box5').hasClass('oppDog') && $('#box7').hasClass('oppDog')) {
-        alert ('dogs rule, cats drools');
-        updateDogScore();
-    } else if ($('.firstPlayerCat').length + $('.oppDog').length === 9) {
-        alert('tic tac tie');
-        reset();
-     }
+  if ($('#box1').hasClass('firstPlayerCat') && $('#box2').hasClass('firstPlayerCat') && $('#box3').hasClass('firstPlayerCat')) {
+    catWinMsg();
+  } else if ($('#box4').hasClass('firstPlayerCat') && $('#box5').hasClass('firstPlayerCat') && $('#box6').hasClass('firstPlayerCat')) {
+      catWinMsg();
+  } else if ($('#box7').hasClass('firstPlayerCat') && $('#box8').hasClass('firstPlayerCat') && $('#box9').hasClass('firstPlayerCat')) {
+      catWinMsg();
+  } else if ($('#box1').hasClass('firstPlayerCat') && $('#box4').hasClass('firstPlayerCat') && $('#box7').hasClass('firstPlayerCat')) {
+      catWinMsg();
+  } else if ($('#box2').hasClass('firstPlayerCat') && $('#box5').hasClass('firstPlayerCat') && $('#box8').hasClass('firstPlayerCat')) {
+      catWinMsg();
+  } else if ($('#box3').hasClass('firstPlayerCat') && $('#box6').hasClass('firstPlayerCat') && $('#box9').hasClass('firstPlayerCat')) {
+      catWinMsg();
+  } else if ($('#box1').hasClass('firstPlayerCat') && $('#box5').hasClass('firstPlayerCat') && $('#box9').hasClass('firstPlayerCat')) {
+      catWinMsg();
+  } else if ($('#box3').hasClass('firstPlayerCat') && $('#box5').hasClass('firstPlayerCat') && $('#box7').hasClass('firstPlayerCat')) {
+      catWinMsg();
+  } else if ($('#box4').hasClass('oppDog') && $('#box5').hasClass('oppDog') && $('#box6').hasClass('oppDog')) {
+      dogWinMsg();
+  } else if ($('#box1').hasClass('oppDog') && $('#box2').hasClass('oppDog') && $('#box3').hasClass('oppDog')) {
+      dogWinMsg()
+  } else if ($('#box7').hasClass('oppDog') && $('#box8').hasClass('oppDog') && $('#box9').hasClass('oppDog')) {
+      dogWinMsg()
+  } else if ($('#box1').hasClass('oppDog') && $('#box4').hasClass('oppDog') && $('#box7').hasClass('oppDog')) {
+      dogWinMsg()
+  } else if ($('#box2').hasClass('oppDog') && $('#box5').hasClass('oppDog') && $('#box8').hasClass('oppDog')) {
+      dogWinMsg()
+  } else if ($('#box3').hasClass('oppDog') && $('#box6').hasClass('oppDog') && $('#box9').hasClass('oppDog')) {
+      dogWinMsg()
+  }  else if ($('#box1').hasClass('oppDog') && $('#box5').hasClass('oppDog') && $('#box9').hasClass('oppDog')) {
+      dogWinMsg()
+  } else if ($('#box3').hasClass('oppDog') && $('#box5').hasClass('oppDog') && $('#box7').hasClass('oppDog')) {
+      dogWinMsg()
+  } else if ($('.firstPlayerCat').length + $('.oppDog').length === 9) {
+      tieMsg();
+      setTimeout(reset, 2000);
+  }
 }
 
 $resetBtn.on('click', reset);
